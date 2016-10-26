c64fe148d1e70fd9c707b071b9a2c8e1eae294fd
Toshbrown <toshbrown@gmail.com>
2016-10-26 09:55:04 +0100

    A bit of a refactor. Timeseries storage is now available on /api/data and /api/ts. /apt/ts will become the new default

--- src/main.js
+++ src/main.js
@@ -1,49 +1,26 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var databox_directory = require("./utils/databox_directory.js");
-var request = require('request'); 
 
 var timeseriesRouter = require('./timeseries.js');
 var keyValueRouter = require('./keyvalue.js');
+var actuateRouter = require('./actuate.js');
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//TODO app.use(Macaroon checker);
 
+//TODO app.use(Macaroon checker);
 
 app.get("/status", function(req, res) {
     res.send("active");
 });
 
-app.post('/api/actuate', function(req, res, next) {
-    var actuator_id = req.body.actuator_id;     
-    databox_directory.get_driver_hostname_from_actuator_id(actuator_id)
-    .then((hostname) =>{
-        var options = {
-            uri: 'http://'+hostname+':8080/api/actuate',
-            method: 'POST',
-            json: {
-                actuator_id: actuator_id,
-                method:req.body.method,
-                data:req.body.data
-            }
-        };
-        console.log("Passing through call to /actuate", options);
-        request(options, function (error, response, body) {
-            if (error) {
-                res.send(error);
-                return;
-            }
-            res.send(body);
-        });
-    })
-    .catch((err)=>{ console.log('[ERROR] /actuate ', err); res.send(error);})
-});
+app.use('/api/actuate',actuateRouter(app));
 
-app.use('/api/data',timeseriesRouter(app));
+app.use('/:var(api/data|api/ts)?',timeseriesRouter(app));
 
 app.use('/api/key',keyValueRouter(app));
 
