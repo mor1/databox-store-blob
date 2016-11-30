a3326e714233a05541806157fc6596553101f972
Toshbrown <toshbrown@gmail.com>
2016-11-30 12:10:08 +0000

    macaroon wip

---
+++ src/lib/databox-request/databox-request.js
@@ -0,0 +1,69 @@
+
+
+module.exports = function () {
+  
+  var request = require('request');
+  var https = require('https');
+
+  const ARBITER_TOKEN   = process.env.ARBITER_TOKEN
+
+  const CM_HTTPS_CA_ROOT_CERT = process.env.CM_HTTPS_CA_ROOT_CERT || '';
+  
+  var agentOptions = {};
+  
+  if(CM_HTTPS_CA_ROOT_CERT === '') {
+    console.log("WARNING:: no https root cert provided not checking https certs.")
+    agentOptions.rejectUnauthorized = false
+  } else {
+     agentOptions.ca = CM_HTTPS_CA_ROOT_CERT
+  };
+
+  var httpsAgent = new https.Agent(agentOptions);
+
+  var databoxRequest = {};
+
+  databoxRequest.get = function (url,options) {
+    return new Promise((resolve, reject) => {
+      console.log("GETTING::", url);
+      opt = options || {};
+      opt.url = url;
+      opt.method = 'GET';
+      opt.agent = httpsAgent;
+      opt.headers = { "X-Api-Key": ARBITER_TOKEN };
+
+      request(opt,function(error, response, body){
+          console.log(error, response.statusCode, body)
+          if(error !== null) {
+            reject(error, response, body);
+          } else if (response.statusCode != 200) {
+            reject(body, response, body);
+          } else {
+            resolve(error, response, body)
+          }
+        });
+    });
+  }
+
+  databoxRequest.post = function (url,options) {
+    return new Promise((resolve, reject) => {
+      opt = options || {};
+      opt.url = url;
+      opt.method = 'POST';
+      opt.agent = httpsAgent;
+      opt.headers = { "X-Api-Key": ARBITER_TOKEN };
+
+      request(opt,function(error, response, body){
+          if(error !== null) {
+            reject(error, response, body);
+          } else if (response.statusCode != 200) {
+            reject(body, response, body);
+          } else {
+            resolve(error, response, body)
+          }
+        });
+    });
+  }
+
+  return databoxRequest;
+  
+}
--- src/main.js
+++ src/main.js
@@ -3,19 +3,29 @@ var https = require('https');
 var express = require("express");
 var bodyParser = require("body-parser");
 
+// A request wrapper that inserts the container mangers root cert and
+// arbiter api key into GET and POST requests
+var request = require('./lib/databox-request/databox-request.js')();
+
+var macaroons = require('macaroons.js');
+
 var timeseriesRouter = require('./timeseries.js');
 var keyValueRouter = require('./keyvalue.js');
 var actuateRouter = require('./actuate.js');
 var hypercat = require('./lib/hypercat/hypercat.js');
 
-var DATABOX_LOCAL_NAME = process.env.DATABOX_LOCAL_NAME || "databox-store-blob";
+const DATABOX_LOCAL_NAME = process.env.DATABOX_LOCAL_NAME || "databox-store-blob";
 
-var HTTPS_CLIENT_CERT = process.env.HTTPS_CLIENT_CERT || '';
-var HTTPS_CLIENT_PRIVATE_KEY = process.env.HTTPS_CLIENT_PRIVATE_KEY || '';
-var credentials = {
+const DATABOX_ARBITER_ENDPOINT = process.env.DATABOX_ARBITER_ENDPOINT || "https://databox-arbiter:8080" 
+
+//HTTPS certs georated by the container mangers for this components HTTPS server.
+const HTTPS_CLIENT_CERT = process.env.HTTPS_CLIENT_CERT || '';
+const HTTPS_CLIENT_PRIVATE_KEY = process.env.HTTPS_CLIENT_PRIVATE_KEY || '';
+const credentials = {
 	key:  HTTPS_CLIENT_PRIVATE_KEY,
 	cert: HTTPS_CLIENT_CERT,
 };
+
 
 var app = express();
 
@@ -32,7 +42,25 @@ var databoxLogger = require('./lib/log/databox-log-middelware.js')(logsDb);
 app.use(databoxLogger);
 
 
-//TODO app.use(Macaroon checker);
+//Macaroon checker TODO move this into a module?
+app.use(function (req, res, next) {
+    var mac = null
+    if('macaroon' in req.body) {
+      mac = req.body.macaroon;
+      delete req.body.macaroon
+    } else if ('macaroon' in req.query) {
+      mac = req.query.macaroon;
+      delete req.query.macaroon
+    } else {
+      res.status(400).send('Missing macaroon');
+      return
+    }
+
+    macaroon = macaroons.MacaroonsBuilder.deserialize(mac);
+    req.macaroon = new macaroons.MacaroonsVerifier(macaroon).satisfyExact("target = " + DATABOX_LOCAL_NAME);
+
+    next();
+})
 
 app.get("/status", function(req, res) {
     res.send("active");
@@ -67,7 +95,23 @@ var WebSocketServer = require('ws').Server;
 app.wss = new WebSocketServer({ server: server });
 app.broadcastDataOverWebSocket = require('./lib/websockets/broadcastDataOverWebSocket.js')(app);
 
-server.listen(8080,function() {
-    console.log("listening on 8080");
-});
+
+//Register with arbiter and get secret
+app.arbiterSecret = null;
+
+request.get(DATABOX_ARBITER_ENDPOINT+'/store/secret')
+    .then((error, response, body) => {
+        console.log(error,response,body)
+        app.arbiterSecret = Buffer.from(body,'base64');
+
+        server.listen(8080,function() {
+                console.log("listening on 8080");
+            });
+    })
+    .catch((error)=>{
+        console.log(error);
+        throw new Error(error);
+    });
+
+
 module.exports = app;
